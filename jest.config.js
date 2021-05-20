4fea0cc67dbc9789c4235c5173fdb16f2d7d195c
Rich <rclt201@gmail.com>
2021-05-20 19:44:40 +0100

    fix: eslint root files

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,18 @@
-const { defaults } = require("jest-config")
+const { defaults } = require('jest-config');
 
 module.exports = {
-  setupFilesAfterEnv: ["./jest.setup.ts"],
-  moduleFileExtensions: [
-    ...defaults.moduleFileExtensions,
-    "ts",
-    "tsx",
-    "js",
-    "json",
-  ],
-  testPathIgnorePatterns: ["node_modules/"],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx', 'js', 'json'],
+  testPathIgnorePatterns: ['node_modules/'],
   transform: {
-    ".*": "babel-jest",
+    '.*': 'babel-jest',
   },
-  testMatch: ["**/*.test.(ts|tsx)"],
-  testEnvironment: "jsdom",
+  testMatch: ['**/*.test.(ts|tsx)'],
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     // Mocks out all these file formats when tests are run
-    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "identity-obj-proxy",
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'identity-obj-proxy',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-}
+};
